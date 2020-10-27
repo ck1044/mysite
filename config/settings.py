d99c8aeca159b0e2ed1666b97a4ea7fac006413f
ck1044 <kyum1044@gmail.com>
2020-10-27 10:34:06 +0900

    second

--- config/settings.py
+++ config/settings.py
@@ -103,10 +103,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# ---------------------------------------- [edit] ---------------------------------------- #
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
-
+TIME_ZONE = 'Asia/Seoul'
+# ---------------------------------------- [edit] ---------------------------------------- #
 USE_I18N = True
 
 USE_L10N = True
