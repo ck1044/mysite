467b882b7cf7c3a7e785d4cfe10ba06ddb83a069
ck1044 <kyum1044@gmail.com>
2020-11-08 17:51:01 +0900

    2-10

--- pybo/views.py
+++ pybo/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Qusetion
+from .forms import QuestionForm
 
 
 def index(request):
@@ -31,6 +32,13 @@ def answer_create(request, question_id):
     question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
     return redirect('pybo:detail', question_id=question.id)
 
+
+def question_create(request):
+    """
+    pybo 질문 등록
+    """
+    form = QuestionForm
+    return
 # ---------------------------------------- [edit] ---------------------------------------- #
 
 
